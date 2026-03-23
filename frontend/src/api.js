4bbabc012754555f279309a168c75cd84fd97764
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2026-03-23 16:00:00 +0530

    add api client

---
+++ frontend/src/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '',
+  timeout: 120000,
+});
+
+export async function uploadPDF(file) {
+  const formData = new FormData();
+  formData.append('file', file);
+  const response = await api.post('/upload', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+  return response.data;
+}
+
+export async function queryDocument(question, chatHistory = []) {
+  const response = await api.post('/query', {
+    question,
+    chat_history: chatHistory,
+  });
+  return response.data;
+}
+
+export async function summarizeDocuments() {
+  const response = await api.post('/summarize');
+  return response.data;
+}
